e8b8b0b624a29e71b2191841f2a8fcd97e3ca330
freitzzz <1160907@isep.ipp.pt>
2024-04-08 20:29:46 +0100

    feat: setup logging on server and browser startup with lumberdash and hooks

---
+++ src/lib/logging/client.ts
@@ -0,0 +1,4 @@
+import { EmojiLumberdashClient } from '@web-pacotes/lumberdash';
+
+export class ServerLumberdashClient extends EmojiLumberdashClient {}
+export class BrowserLumberdashClient extends EmojiLumberdashClient {}
