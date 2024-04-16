ec0627e503a00d604c1424de0864f0856b39f9bb
freitzzz <1160907@isep.ipp.pt>
2024-04-16 19:28:12 +0100

    docs: ts-doc for Todo type

--- src/lib/models/todo.ts
+++ src/lib/models/todo.ts
@@ -1,4 +1,18 @@
-export interface Todo {
-	id: string;
-	value: string;
+import { nanoid } from 'nanoid';
+
+/**
+ * Creates a new [Todo].
+ *
+ * @param value - the content of the todo.
+ */
+export function Todo(value: string) {
+	return {
+		id: nanoid(),
+		value: value
+	};
 }
+
+/**
+ * Models a Todo type.
+ */
+export type Todo = ReturnType<typeof Todo>;
