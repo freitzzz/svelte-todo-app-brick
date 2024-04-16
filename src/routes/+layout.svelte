ccb81b1d9354c3dc8aefa38c49725a15e33dfda5
freitzzz <1160907@isep.ipp.pt>
2024-04-16 19:26:30 +0100

    feat: provide todos reactor on root component render

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
 	import '../app.css';
+	import { vault } from '@core';
+	import { ReactorProvider } from '@web-pacotes/reactor-svelte';
+	import { TodoFetched, TodoReactor } from '@reactors';
+
+	const appVault = vault(window?.window);
+
+	const todos = new TodoReactor(appVault.todosRepository);
+	todos.add(TodoFetched());
 </script>
 
-<slot />
+<ReactorProvider reactor={todos}>
+	<slot />
+</ReactorProvider>
