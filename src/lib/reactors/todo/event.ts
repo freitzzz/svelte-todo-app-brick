56961b737271ecffec706b305e9e17c6e6739a33
freitzzz <1160907@isep.ipp.pt>
2024-04-16 19:02:59 +0100

    feat: implement map of fetch todos event to state

--- src/lib/reactors/todo/event.ts
+++ src/lib/reactors/todo/event.ts
@@ -8,7 +8,15 @@ export function TodoCreated(value: Todo) {
 	};
 }
 
+export function TodoFetched() {
+	return {
+		type: 'todo-fetched'
+	};
+}
+
 export type TodoCreated = ReturnType<typeof TodoCreated>;
-export type TodoEvent = TodoCreated;
+export type TodoFetched = ReturnType<typeof TodoFetched>;
+export type TodoEvent = TodoCreated | TodoFetched;
 
 export const isTodoCreated = (event: TodoEvent) => isTypedOf<TodoCreated>(event, 'todo-created');
+export const isTodoFetched = (event: TodoEvent) => isTypedOf<TodoFetched>(event, 'todo-fetched');
--- src/lib/reactors/todo/reactor.ts
+++ src/lib/reactors/todo/reactor.ts
@@ -1,13 +1,51 @@
+import {
+	isTodoCreated,
+	isTodoFetched,
+	TodoCreated,
+	type TodoEvent,
+	type TodoFetched
+} from './event';
+import { TodoInitial, type TodoState, TodoUpdate } from './state';
+import { logError, logMessage } from '@web-pacotes/lumberdash';
+import type { TodosRepository } from '@data';
 import { Reactor } from '@web-pacotes/reactor-svelte';
-import { isTodoCreated, TodoCreated, type TodoEvent } from './event';
-import { TodoInitial, TodoUpdate, type TodoState } from './state';
+import { fold, wrap } from '@web-pacotes/foundation-types';
 
+/**
+ * A reactor that manages the state of todo actions.
+ */
 export class TodoReactor extends Reactor<TodoEvent, TodoState> {
-	constructor() {
-		super(TodoInitial());
+	constructor(repository: TodosRepository) {
+		super(TodoInitial([]));
 
-		this.on<TodoCreated>((event, emit) => {
-			emit(TodoUpdate([...this.state.value, event.value]));
+		this.on<TodoCreated>(async (event, emit) => {
+			const todo = event.value;
+
+			logMessage(`Received new todo: ${todo.value}`);
+
+			if (todo.value.trim().length > 0) {
+				await repository.add(todo);
+
+				emit(TodoUpdate([...this.state.value, event.value]));
+			}
 		}, isTodoCreated);
+
+		this.on<TodoFetched>(async (event, emit) => {
+			logMessage(`Fetching all todos...`);
+
+			const result = await repository.all();
+
+			emit(
+				fold(
+					result,
+					(l) => {
+						logError(wrap(l));
+
+						return this.state;
+					},
+					(r) => TodoUpdate(r)
+				)
+			);
+		}, isTodoFetched);
 	}
 }
