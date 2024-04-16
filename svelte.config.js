e44a06a366e3be83c14afcfdb52b9263059782c7
freitzzz <1160907@isep.ipp.pt>
2024-04-16 19:27:28 +0100

    chore: add logging, data, core, reactors and models barrel files alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,22 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 import adapter from '@sveltejs/adapter-node';
-
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
-
 		alias: {
-			'@components': path.resolve('./src/lib/components/index.ts'),
-			'@http': path.resolve('./src/lib/http/index.ts'),
-			'@stores': path.resolve('./src/lib/stores/index.ts'),
+			'@presentation': path.resolve('./src/lib/presentation/index.ts'),
+			'@presentation/components': path.resolve('./src/lib/presentation/components'),
+			'@logging': path.resolve('./src/lib/logging/index.ts'),
+			'@data': path.resolve('./src/lib/data/index.ts'),
+			'@core': path.resolve('./src/lib/core/index.ts'),
+			'@reactors': path.resolve('./src/lib/reactors/index.ts'),
+			'@models': path.resolve('./src/lib/models/index.ts'),
 			'@i18n': path.resolve('./src/i18n/index.ts')
 		}
 	}
