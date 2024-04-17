0548e70e1f38313aac363752ede7494fdf1e4030
freitzzz <1160907@isep.ipp.pt>
2024-04-17 08:11:48 +0100

    feat: initial page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
-<script>
-	import { CardNoImage, CardWithImage, Heading, RedBall } from '@components';
-	import { LL } from '@i18n';
+<script lang="ts">
+	import { TodoList } from '@presentation';
+	import { TodoCreated, TodoReactor, TodosDeleted, TodoUpdated } from '@reactors';
+	import { logMessage } from '@web-pacotes/lumberdash';
+	import { ReactorListener, resolve } from '@web-pacotes/reactor-svelte';
+
+	const todos = resolve(TodoReactor);
 </script>
 
-<Heading value="svelte-todo-app-brick" />
-<div class="flex flex-row justify-evenly">
-	<CardNoImage />
-	<CardWithImage />
-</div>
-<div class="flex flex-col justify-center">
-	<RedBall />
-	<p class="text-center">{$LL.greetings()}</p>
+<div class="flex justify-center">
+	<ReactorListener reactor={todos} listener={(state) => logMessage(`new state: ${state.type}`)}>
+		<TodoList
+			values={$todos.value}
+			onNewTodo={(value) => todos.add(TodoCreated(value))}
+			onUpdateTodo={(value) => todos.add(TodoUpdated(value))}
+			onDeleteTodos={() => todos.add(TodosDeleted())}
+		/>
+	</ReactorListener>
 </div>
