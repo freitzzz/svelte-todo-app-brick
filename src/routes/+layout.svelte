1590245c411d99c1738ec641b5bd9ab001a8f3f2
freitzzz <1160907@isep.ipp.pt>
2024-04-18 10:21:46 +0100

    feat: provide alerts reactor on root layout and render toast group next to root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,21 @@
 <script>
 	import '../app.css';
 	import { vault } from '@core';
-	import { ReactorProvider } from '@web-pacotes/reactor-svelte';
-	import { TodoFetched, TodoReactor } from '@reactors';
+	import { MultiReactorProvider } from '@web-pacotes/reactor-svelte';
+	import { TodoFetched, TodoReactor, AlertsReactor } from '@reactors';
 	import { browser } from '$app/environment';
+	import { ToastGroup } from '@presentation';
 
 	const appVault = vault(browser ? window.window : undefined);
 
 	const todos = new TodoReactor(appVault.todosRepository);
+	const alerts = new AlertsReactor();
+
 	todos.add(TodoFetched());
 </script>
 
-<ReactorProvider reactor={todos}>
+<MultiReactorProvider reactors={[todos, alerts]}>
 	<slot />
-</ReactorProvider>
+
+	<ToastGroup values={$alerts.value} />
+</MultiReactorProvider>
