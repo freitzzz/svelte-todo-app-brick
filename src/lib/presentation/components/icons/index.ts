6929ddbab41a6548f389a8e027809402c38b361e
freitzzz <1160907@isep.ipp.pt>
2024-05-02 23:45:56 +0100

    feat: add moon, sun and flower icons

--- src/lib/presentation/components/icons/index.ts
+++ src/lib/presentation/components/icons/index.ts
@@ -1,2 +1,3 @@
-export { default as Check } from './check.svelte';
-export { default as Minus } from './minus.svelte';
+export { default as Flower } from './flower.svelte';
+export { default as Moon } from './moon.svelte';
+export { default as Sun } from './sun.svelte';
