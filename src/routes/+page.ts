fe72dca9b5471671c74678ec2eb904c9dd507c26
freitzzz <1160907@isep.ipp.pt>
2024-05-03 18:56:58 +0100

    feat: use navigator.language detector for csr, accept language detector on ssr

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,10 @@
 import { detectLocale, loadLocaleAsync, setLocale } from '@i18n';
 import type { PageLoad } from './$types';
+import { navigatorDetector } from 'typesafe-i18n/detectors';
+import { browser } from '$app/environment';
 
 export const load = (async () => {
-	const locale = detectLocale();
+	const locale = browser ? detectLocale(navigatorDetector) : detectLocale();
 
 	await loadLocaleAsync(locale);
 
